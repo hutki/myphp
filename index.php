474c39326c8b741dcfb106e30c636f1df1bcab2f
hutki <hutki@tut.by>
2017-02-22 22:08:48 +0300

    3 Урока

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head lang="ru">
+    <meta charset="UTF-8">
+    <title>Регистрал</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="Регистрал">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <?php
+        $name  = "Виталик";
+        $age = 37;
+        echo "$name";
+        echo '<br>dfgsg';
+        echo "<br> Моего друга зовут $name, ему $age лет";
+        $n = 10;
+
+        $c = $age + $n;
+
+        echo "<br>Александру через 10 лет будет $c лет";
+
+
+    ?>
+</body>
+</html>
